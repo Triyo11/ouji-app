1cd0035258d79933475aaf030c1a66b040272f1d
Triyo11 <rusdiantoro117@gmail.com>
2024-10-20 14:31:55 +0700

    remove project from setting overlay menu

--- src/components/Sidebar/settingOptions.ts
+++ src/components/Sidebar/settingOptions.ts
@@ -2,11 +2,6 @@ import { signOut } from "next-auth/react";
 
 const settingOptions = [
   {
-    label: "Projects",
-    type: "direction",
-    href: "/project",
-  },
-  {
     label: "Sign out",
     type: "method",
     action: () => signOut(),
